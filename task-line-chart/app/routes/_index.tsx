f098a5902f1c3d1fad449c920a84252a69fe31fc
rehmanshams <rehman.alphasquad@gmail.com>
2024-09-19 18:22:11 +0500

    updated git file locatio

--- task-line-chart/app/routes/_index.tsx
+++ task-line-chart/app/routes/_index.tsx
@@ -1,6 +1,3 @@
-import type { MetaFunction } from "@remix-run/node";
-import { useEffect } from "react";
-import useWebSocket, { ReadyState } from "react-use-websocket";
 import { ClientOnly } from "remix-utils/client-only";
 import { CoinChart } from "~/components/CoinChart";
 
